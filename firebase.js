e07a4fbb6cbd327b26b35e3c6564223d93aeb0cd
lahyabder <120222161+lahyabder@users.noreply.github.com>
2025-11-25 03:12:43 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+// نفس الإعدادات التي أعطيتني إياها
+const firebaseConfig = {
+  apiKey: "AIzaSyCtuWAOF9bCaPGosu0M44NtgL8XhzxNWec",
+  authDomain: "sleepshare-fbe2a.firebaseapp.com",
+  projectId: "sleepshare-fbe2a",
+  storageBucket: "sleepshare-fbe2a.firebasestorage.app",
+  messagingSenderId: "357111824676",
+  appId: "1:357111824676:web:a425f1db67ea57bef3f3b1",
+  measurementId: "G-9BTE9JG8NC"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
